0f7c5dfbd91b900dbed42e68ce938de5d0aef5d6
AvishayAv <avishaya67@gmail.com>
2021-11-16 15:51:44 +0200

    Rule base for start_date and end_date, by grepping typical regexes and figuring them out (handle date ranges, missing year, different formats, etc...)

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import os.path
+import re
+
+from datetime import datetime
+import dateutil.parser as dparser
+
 from facebook_scraper import get_posts
 import pickle
 import datetime
 from Sublet import Sublet
 import time
 from FacebookGroup import FacebookGroups
+from DateParser import DatePatterns, DateReg
 
 
 def get_data_from_facebook():
@@ -15,21 +22,42 @@ def get_data_from_facebook():
     for fb_group in fb_groups_list_private:
         for post in get_posts(group=fb_group, pages=5, credentials=credentials, options={"progress": True, "posts_per_page": 100}): # TODO : change number of pages and posts per page + add comments?
             list_of_sublets.append(post)
-        time.sleep(120)
+        time.sleep(500)
     for fb_group in fb_groups_list_public:
         for post in get_posts(group=fb_group, pages=5, options={"progress": True, "posts_per_page": 100}): # TODO : change number of pages and posts per page + add comments?
             list_of_sublets.append(post)
-        time.sleep(120)
+        time.sleep(500)
     return list_of_sublets
 
+# do not use datefinder - not working well with hebrew
+def extract_dates_from_text(text):
+    dates = []
+    for date_pattern in DatePatterns().patterns:
+        dates_regex = [DateReg(x, date_pattern, "." in x) for x in re.findall(date_pattern.pattern, text)]
+        for date_regex in dates_regex:
+            date_regex.complete_year()
+        dates.extend(dates_regex)
+        if (date_pattern.name.startswith('combined') and len(dates) >= 1) or len(dates) >= 2:
+            break
 
-def parse_data_from_facebook(list_of_sublets):
+    # Prioritize ranged dates
+    patterns = [d.date_pattern.is_range for d in dates] # TODO [AA] - 1. handle empty case  2. handle ranged case
+    if True in patterns or len(patterns) == 0:
+        return 'a', 'b'
+
+    real_dates = [dparser.parse(i.date, fuzzy=True, dayfirst=True).date() for i in dates]
+    return min(real_dates), max(real_dates)
+
+
+def parse_data_from_facebook(dict_of_sublets):
+    list_of_sublets = []
+    for v in dict_of_sublets.values():
+        list_of_sublets.extend(v)
     sublets = []
     for sublet in list_of_sublets:
         post_url = sublet['post_url']
         post_time = sublet['time']
-        start_date = ''  # TODO : parse start_date from text
-        end_date = ''  # TODO : parse end_date from text
+        start_date, end_date = extract_dates_from_text(sublet['text']) # TODO [AA] : should be list (in case of multiple date options)
         location = sublet.get(
             'listing_location')  # TODO : 1.if None-parse from text, if number-figure out what is this number and decide
         rooms = 0  # TODO : parse rooms from text
@@ -43,9 +71,11 @@ def parse_data_from_facebook(list_of_sublets):
 
 
 def facebook():
-    list_of_sublets = get_data_from_facebook()
-    pickle.dump(list_of_sublets, open('list_of_sublets.p', 'wb'))
-    # list_of_sublets = pickle.load(open("list_of_sublets.p", 'rb')) # Dummy for time saving
+    if os.path.exists('dict_of_sublets.p'): # Dummy for time saving
+        list_of_sublets = pickle.load(open('dict_of_sublets.p','rb'))
+    else:
+        list_of_sublets = get_data_from_facebook()
+        pickle.dump(list_of_sublets, open('dict_of_sublets.p', 'wb'))
     return parse_data_from_facebook(list_of_sublets)
 
 
