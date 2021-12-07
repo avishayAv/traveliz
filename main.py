f48a53a68ef62242f22c5129b056c8b093224f0d
rohi281 <rohi281@gmail.com>
2021-12-07 17:33:18 +0200

    useragent rotation

--- main.py
+++ main.py
@@ -19,24 +19,6 @@ from lxml.html import fromstring
 from itertools import cycle
 
 
-def get_proxy_list():
-    url = 'https://free-proxy-list.net/'
-    response = requests.get(url)
-    parser = fromstring(response.text)
-    proxies = set()
-    for i in parser.xpath('//tbody/tr')[:1000]:
-        if i.xpath('.//td[7][contains(text(),"yes")]'):
-        #Grabbing IP and corresponding PORT
-            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
-            proxies.add(proxy)
-    return proxies
-
-
-def get_proxy(proxies):
-    proxytosend = random.choice(list(proxies))
-    return proxytosend
-
-
 def get_useragent_list():
     software_names = [SoftwareName.CHROME.value]
     operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
@@ -52,7 +34,7 @@ def get_useragent(useragents):
     return usergentsend
 
 
-def get_data_from_facebook(proxies, useragents, already_done):
+def get_data_from_facebook(already_done):
     fb_groups = FacebookGroups().groups
     fb_groups_list_private = [group.group_id for group in fb_groups if not group.is_public]
     fb_groups_list_public = [group.group_id for group in fb_groups if group.is_public]
@@ -68,20 +50,12 @@ def get_data_from_facebook(proxies, useragents, already_done):
 #            time.sleep(random.randint(0, 200))
     for fb_group in fb_groups_list_public:
         if fb_group not in already_done:
-            a = get_proxy(proxies)
-            print(a)
-            set_proxy(a)
-            b = get_useragent(useragents)
-            print(b)
-            set_user_agent(b)
-            c = get_header()
-            print(c)
             group_posts = []
             for post in get_posts(group=fb_group, pages=random.randint(1, 2), #credentials=random.choice(credentials),
                                   options={"progress": True, "posts_per_page": random.randint(50, 100)}):  # TODO : change number of pages and posts per page + add comments?
                 group_posts.append(post)
             yield fb_group, group_posts
-            #time.sleep(random.randint(0, 200))
+           # time.sleep(random.randint(0, 200))
 
 
 def parse_data_from_facebook(dict_of_sublets):
@@ -114,9 +88,9 @@ def parse_data_from_facebook(dict_of_sublets):
     return sublets
 
 
-def facebook(proxies, useragents):
+def facebook():
     dict_of_sublets = pickle.load(open("dict_of_sublets.p", 'rb')) if os.path.exists("dict_of_sublets.p") else {}
-    for group_id, group_posts in tqdm(get_data_from_facebook(proxies, useragents, already_done=set(dict_of_sublets.keys())),
+    for group_id, group_posts in tqdm(get_data_from_facebook(already_done=set(dict_of_sublets.keys())),
                                        desc='extracting groups data'):
          dict_of_sublets[group_id] = group_posts
          pickle.dump(dict_of_sublets, open('dict_of_sublets.p', 'wb'))
@@ -124,12 +98,8 @@ def facebook(proxies, useragents):
 
 
 def main():
-    proxies = get_proxy_list()
-    useragents = get_useragent_list()
-    c = get_header()
-    print(c)
     sublets = []
-    sublets.extend(facebook(proxies,useragents))
+    sublets.extend(facebook())
     pass
 
 
